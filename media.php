9b680958638ec23de21415aab26823c0d3a86e46
liaaanggraini0303 <liaaanggraini030904@gmail.com>
2024-06-05 20:45:46 +0700

    tambahkan modul

--- media.php
+++ media.php
@@ -1,37 +1,34 @@
 <?php
 if(!isset($_GET['modul'])){
     include_once('modul/home/index.php');
-}else
-     if($_GET["modul"]=="transaksi"){ 
-        include_once('modul/transaksi/index.php'); 
+}else if($_GET['modul']=="transaksi"){
+    include_once('modul/transaksi/index.php');
+}else if($_GET['modul']=="akun"){
+    include_once('modul/akun/index.php');
+}else if($_GET['modul']=="pembelian"){
+    include_once('modul/pembelian/index.php');
+}else if($_GET['modul']=="pembayaran"){
+    include_once('modul/pembayaran/index.php');
+}else if($_GET['modul']=="penjualan"){
+    include_once('modul/penjualan/index.php');
+}else if($_GET['modul']=="pelanggan"){
+    include_once('modul/pelanggan/index.php');
+}else if($_GET['modul']=="barang"){
+    include_once('modul/barang/index.php');
+}else if($_GET['modul']=="suplier"){
+    include_once('modul/suplier/index.php');
+}else if($_GET['modul']=="pengguna"){
+    include_once('modul/pengguna/index.php');
+}else if($_GET['modul']=="jurnal"){
+    include_once('modul/jurnal/index.php');
+}else if($_GET['modul']=="profile"){
+    include_once('modul/profile/index.php');
 }else{
-     if($_GET["modul"]=="akun"){ 
-        include_once('modul/akun/index.php');
-    }elseif($_GET['modul']=="pembelian"){ 
-        include_once('modul/pembelian/index.php');
-    }elseif($_GET['modul']=="pembayaran"){ 
-        include_once('modul/pembayaran/index.php');
-    }elseif($_GET['modul']=="penjualan"){
-        include_once('modul/penjualan/index.php');
-    }elseif($_GET['modul']=="pelanggan"){
-        include_once('modul/pelanggan/index.php');
-    }elseif($_GET['modul']=="barang"){
-        include_once('modul/barang/index.php');
-    }elseif($_GET['modul']=="suplier"){
-        include_once('modul/suplier/index.php');
-    }elseif($_GET['modul']=="pengguna") {
-        include_once('modul/pengguna/index.php');
-    }elseif($_GET['modul']=="jurnal"){
-        include_once('modul/jurnal/index.php');
-    }elseif($_GET['modul']=="profile"){
-        include_once('modul/profile/index.php');
-    }else{ 
     ?>
-    <div class="alert alert-info"> 
-        <strong>Perhatian : </strong>
+     <div class="alert alert-info">
+        <strong>perhatian : </strong>
         Modul yang anda request tidak ada!!!
-     </div>
+    </div>
     <?php
-    }
+
 }
-?>
